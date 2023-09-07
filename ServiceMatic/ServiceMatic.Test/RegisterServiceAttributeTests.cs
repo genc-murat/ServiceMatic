e24840d4815c1127c74306930d5b60c18f2ae99f
Murat GENÇ <muttafi@gmail.com>
2023-09-07 14:41:33 +0300

    RegisterServiceAttributeTests

---
+++ ServiceMatic/ServiceMatic.Test/RegisterServiceAttributeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceMatic.Test;
+
+public class RegisterServiceAttributeTests
+{
+    [Fact]
+    public void Constructor_ShouldSetLifetimeProperty()
+    {
+        // Arrange & Act
+        var attribute = new RegisterServiceAttribute(ServiceLifetime.Singleton);
+
+        // Assert
+        attribute.Lifetime.Should().Be(ServiceLifetime.Singleton);
+    }
+
+    [Fact]
+    public void Attribute_ShouldBeApplicableToClass()
+    {
+        // Arrange
+        var attributeType = typeof(RegisterServiceAttribute);
+
+        // Act
+        var attributes = Attribute.GetCustomAttributes(typeof(SampleService), attributeType);
+
+        // Assert
+        attributes.Should().NotBeNullOrEmpty();
+        attributes[0].Should().BeOfType<RegisterServiceAttribute>();
+        ((RegisterServiceAttribute)attributes[0]).Lifetime.Should().Be(ServiceLifetime.Singleton);
+    }
+}
+
+[RegisterService(ServiceLifetime.Singleton)]
+public class SampleService
+{
+}
